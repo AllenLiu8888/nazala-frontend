d2f55b1c5d76446b8beb011a0ec0a0342f17df72
AllenLiu8888 <153785526+AllenLiu8888@users.noreply.github.com>
2025-09-20 16:56:32 +1000

    localhost or server

--- src/services/http.js
+++ src/services/http.js
@@ -1,5 +1,8 @@
 // 网络基础层 - 封装 fetch 和错误处理
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8001';
+const IS_DEV = 0;
+
+const API_BASE_URL = IS_DEV ? 'http://127.0.0.1:8001' : 'http://34.70.141.127:8001';
+
 
 class APIError extends Error {
     constructor(message, status, code) {
