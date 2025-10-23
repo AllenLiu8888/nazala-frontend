0bdf808869cb24b8dcb5ff67e5e0dbedc82f8b0b
AllenLiu8888 <153785526+AllenLiu8888@users.noreply.github.com>
2025-10-23 15:09:12 +1000

    intro content & gameover switch

--- src/components/dashboard/header/WorldStatus.jsx
+++ src/components/dashboard/header/WorldStatus.jsx
@@ -3,7 +3,7 @@
 const WorldStatus = () => {
     // const worldStatus = useGameStore(s => s.turn.questionText);
 
-    const worldStatus = "World Status";
+    const worldStatus = "Stable";
     return (
         <div className="flex-1 flex gap-4 p-8 text-right">
             <h1 className="pixel-text">World Status: {worldStatus}</h1>
